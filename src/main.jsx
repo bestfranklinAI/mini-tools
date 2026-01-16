1a2bfa77609db86178196436c76a9e0be970ea1e
bestfranklinAI <franklin123ann@gmail.com>
2026-01-16 14:31:45 +0800

    PWA implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+// Register service worker for offline support
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    console.log('New content available, please refresh.')
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
